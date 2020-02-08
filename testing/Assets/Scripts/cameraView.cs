4c5f168ff0e448e9f237aaa2d9cab41f2ec84fe1
Steve <steve@truu.id>
2020-02-08 15:21:14 -0500

    Adding more player controls

---
+++ testing/Assets/Scripts/cameraView.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraView : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public float xRotation = 0f;
+    public bool invertRotation = false;
+
+    public Transform playerBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Capture mouse movement
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // Mouse look up and down (+ is inverted, - is normal)
+        xRotation = invertRotation ? xRotation += mouseY : xRotation -= mouseY;
+        // Clamp rotation
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        // Mouse look up and down transformation
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        // Mouse look right and left
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ testing/Assets/Scripts/playerMove.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -14f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.5f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if player is on the ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        Debug.Log(isGrounded);
+
+        // If on the ground reset our velocity value
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        Debug.Log(velocity);
+        // Capture WASD movements on keyboard
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        // Create the movement vector locally
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        // Apply the movement to the Player Controller object
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Calculate Y velocity with gravity to the player
+        // Use formula: dy = 1/2(g)(t^2)
+        velocity.y += gravity * Time.deltaTime;
+        // Apply gravity to player
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
